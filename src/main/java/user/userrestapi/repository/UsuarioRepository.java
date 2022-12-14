1a3f8da4f207d18e64c5b5734b97151707210f6c
Alessandro <alessandronarciso@gmail.com>
2022-12-14 00:03:48 -0300

    Ajuste findById

--- src/main/java/user/userrestapi/repository/UsuarioRepository.java
+++ src/main/java/user/userrestapi/repository/UsuarioRepository.java
@@ -8,15 +8,12 @@ import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
 
-    @Query("SELECT usuario FROM Usuario AS usuario "
-            + " WHERE usuario.id = :idUsuario ")
-    Usuario findById(@Param("idUsuario") long idUsuario);
+    Usuario findById(long idUsuario);
 
     @Query("SELECT usuario FROM Usuario AS usuario "
             + " WHERE (:userName IS NULL OR usuario.userName ilike %:userName%) AND " +
             "(:name IS NULL OR usuario.name ilike %:name%) AND " +
             "(:email IS NULL OR usuario.email ilike %:email%) ")
-
     List<Usuario> filter(@Param("userName") String userName,
                          @Param("name") String name,
                          @Param("email") String email);
